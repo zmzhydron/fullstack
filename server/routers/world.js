65979493c37ca941cf033925d15d52eb9a4b4c60
zmzhydron <zmzhydron@outlook.com>
2020-02-27 23:32:03 +0800

    responseheader & content-type

--- server/routers/world.js
+++ server/routers/world.js
@@ -12,7 +12,11 @@ router.get("/hello", (req, res, next) => {
 router.post('/getphoto', (req, res, next) => {
   let file = fs.readFileSync(`../fileupload/11.jpg`);
   console.log(file, '$$$$$$$');
-  res.setHeader('Content-Type', 'image/jpeg');
+  // res.setHeader('Content-Type', 'image/jpeg');
+  // res.setHeader('Content-Type', 'blob');
+  // res.setHeader("responseType",'blob');
+  // res.setHeader('Content-Type', 'arraybuffer');
+
   let src = path.resolve(__dirname, '../../../fileupload/11.jpg');
   console.log(src);
   // res.sendFile(src);
